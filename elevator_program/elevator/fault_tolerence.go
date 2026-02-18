61af08a2c3d2e20b7308d3dc423d2b30a97d1028
Ida Flakstad <ida.flakstad@gmail.com>
2026-02-18 18:11:30 +0100

    added fault_tolerence file

---
+++ elevator_program/elevator/fault_tolerence.go
@@ -0,0 +1,10 @@
+package elevator
+
+/*
+when not hearing from anybody else:
+how to know if the elevator in question is the issue or the one that it is communicating with
+
+how to make the elevator kill itself and then start anew
+
+how to force another elevator to restart
+*/
